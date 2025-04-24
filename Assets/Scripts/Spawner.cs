8fe942e114cc2dbc87e584205f75eeda8156f840
JasonMadness <tobc_madness@mail.ru>
2025-04-24 13:53:48 +0300

    small fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _prefab;
+    [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private Transform _spawnPoint;
 
     public void GenerateEnemy()
     {
-        Instantiate(_prefab, _spawnPoint.position, transform.rotation);
+        Instantiate(_enemyPrefab, _spawnPoint.position, transform.rotation);
     }
 }
